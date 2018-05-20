06f05afc8885540e98fd62c0842d5366c961dcd2
Julio Pavón <julio@jpavon.com>
2018-05-20 16:48:38 +0200

    style changes

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -4,25 +4,31 @@ import styled from 'styled-components'
 import { IAppState, IStateChange } from 'App'
 import Input from 'components/Input'
 
+const StyledInput = styled(Input)``
+
 const Wrapper = styled.div`
     margin: 0 auto;
     margin-top: 3rem;
     width: 100%;
     max-width: 500px;
+
+    ${StyledInput} {
+        text-align: left;
+    }
 `
 
 const Title = styled.h2`
     margin: 0;
     margin-bottom: 1rem;
     font-size: 1.3rem;
-    text-align: center;
+    /* text-align: center; */
 `
 
 const Subtitle = styled.h2`
     margin: 0;
     margin-bottom: 0.3rem;
     font-size: 1rem;
-    text-align: center;
+    /* text-align: center; */
 `
 
 interface ISettings {
@@ -33,8 +39,8 @@ interface ISettings {
 const Settings: React.SFC<ISettings> = (props) => (
     <Wrapper>
         <Title>Settings</Title>
-        <Subtitle>Training Max:</Subtitle>
-        <Input
+        <Subtitle>Training Max (%):</Subtitle>
+        <StyledInput
             defaultValue={String(props.state.trainingMax)}
             name="trainingMax"
             onChange={(e) =>
