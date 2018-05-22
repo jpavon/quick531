ed1b9fd17d63591350116549fea449864581fc0c
Julio Pavón <julio@jpavon.com>
2018-05-22 20:16:06 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# 531 calculator
-
-todo
--- src/styles/variables.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    font: 'sans-serif',
-    primaryColor: '#555',
-    backgroundColor: '#fff',
-    highlightColor: '#4da2f9',
-    borderColor: '#dedede'
-}
